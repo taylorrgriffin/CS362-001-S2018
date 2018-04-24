d347ec45840f85039230748a6fe91351ef2b6da3
Taylor Griffin <taylor.griffin.365@gmail.com>
2018-04-23 19:29:57 -0700

    removed bash script, target folders, and xmls

--- projects/griftayl/Assignment-2/Calendar/src/main/java/calendar/CalendarMain.java
+++ projects/griftayl/Assignment-2/Calendar/src/main/java/calendar/CalendarMain.java
@@ -19,11 +19,11 @@ public class CalendarMain {
 		System.out.println("Calendar Main: \n" );
 
 
-		
+
 		DataHandler dataHandler;
 		dataHandler = new DataHandler();
 
- 	
+
 		//get todays date
     	Calendar rightnow = Calendar.getInstance();
     	//current month/year/date is today
@@ -33,28 +33,28 @@ public class CalendarMain {
 		int thisDay = rightnow.get(Calendar.DAY_OF_MONTH);
 	   	/** the year the user is currently viewing **/
    	 	int thisYear = rightnow.get(Calendar.YEAR);
-		
+
 //		// Get a calendar which is set to a specified date.
 		Calendar calendar = new GregorianCalendar(thisYear, thisMonth, thisDay);
 	// Increment the calendar's date by 1 day.
 		calendar.add(calendar.DAY_OF_MONTH,1);
-		
-		
-
-    	
 
 
-		
+
+
+
+
+
 		 int startHour=15;
 		 int startMinute=30;
-		 int startDay=thisDay;  	
-		 int startMonth=thisMonth+1; 	
-		 int startYear=thisYear; 	
+		 int startDay=thisDay;
+		 int startMonth=thisMonth+1;
+		 int startYear=thisYear;
 		 String title="Birthday Party";
 		 String description="This is my birthday party.";
 		 String emailAddress="aburasa@oregonstate.edu";
-	
-		 //Construct a new Appointment object with the initial data	 
+
+		 //Construct a new Appointment object with the initial data
          Appt appt1 = new Appt(startHour,
                   startMinute ,
                   startDay ,
@@ -67,16 +67,16 @@ public class CalendarMain {
          System.out.println(appt1.toString());
 
          dataHandler.saveAppt(appt1);
-		
+
          // create another appointment
          startHour=14;
 		 startMinute=30;
-		 startDay=thisDay;  	
-		 startMonth=thisMonth+1; 	
-		 startYear=thisYear; 	
+		 startDay=thisDay;
+		 startMonth=thisMonth+1;
+		 startYear=thisYear;
 		 title="Class";
 		 description="Rescheduled class.";
-		 //Construct a new Appointment object with the initial data	 
+		 //Construct a new Appointment object with the initial data
          Appt appt2 = new Appt(startHour,
                   startMinute ,
                   startDay ,
@@ -85,20 +85,20 @@ public class CalendarMain {
                   title,
                  description,
                  emailAddress);
-  
+
          System.out.println(appt2.toString());
 
          dataHandler.saveAppt(appt2);
-         
+
          // create another appointment
          startHour=16;
 		 startMinute=30;
 		 startDay=calendar.get(Calendar.DAY_OF_MONTH);
-		 startMonth=calendar.get(Calendar.MONTH)+1;	
-		 startYear=thisYear;	
+		 startMonth=calendar.get(Calendar.MONTH)+1;
+		 startYear=thisYear;
 		 title="Visit";
 		 description="Visiting my parents!";
-		 //Construct a new Appointment object with the initial data	 
+		 //Construct a new Appointment object with the initial data
          Appt appt3 = new Appt(startHour,
                   startMinute ,
                   startDay ,
@@ -109,24 +109,24 @@ public class CalendarMain {
                  emailAddress);
          int[] recurDaysArr={2,3,4};
          appt3.setRecurrence( recurDaysArr, Appt.RECUR_BY_WEEKLY, 2, Appt.RECUR_NUMBER_FOREVER);
-         
+
          appt3.setValid();
-         
+
          System.out.println(appt3.toString());
 
-         
+
          dataHandler.saveAppt(appt3);
-     
-  
+
+
          // create another appointment
          startHour=16;
 		 startMinute=30;
-		 startDay=-1;		
-		 startMonth=thisMonth+2;	
-		 startYear=thisYear;	
+		 startDay=-1;
+		 startMonth=thisMonth+2;
+		 startYear=thisYear;
 		 title="Visit";
 		 description="Visiting my parents!";
-		 //Construct a new Appointment object with the initial data	 
+		 //Construct a new Appointment object with the initial data
          Appt appt4 = new Appt(startHour,
                   startMinute ,
                   startDay ,
@@ -137,37 +137,37 @@ public class CalendarMain {
                  emailAddress);
          int[] recurDaysArr4={2,3,4};
          appt4.setRecurrence( recurDaysArr4, Appt.RECUR_BY_WEEKLY, 3, Appt.RECUR_NUMBER_FOREVER);
-         
+
          appt4.setValid();
-         
+
          System.out.println(appt4.toString());
 
-         
+
          dataHandler.saveAppt(appt4);
-         
-         
+
+
  		//get a list of appointments for one day, which is today
  		GregorianCalendar today = new GregorianCalendar(thisYear,thisMonth,thisDay);
  		GregorianCalendar tomorrow = new GregorianCalendar(thisYear,thisMonth,thisDay);
 
  		tomorrow.add(today.DAY_OF_MONTH,1);
- 
+
 
  		DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyy");
 
  //			System.out.println("today is:" + dateFormat.format(today.getTime()));
  //			System.out.println("tomorrow is:" + dateFormat.format(tomorrow.getTime()));
- 			
+
  	        //Create a linked list of calendar days to return
  	        LinkedList<CalDay> calDays = new LinkedList<CalDay>();
 
- 			calDays = (LinkedList<CalDay>) dataHandler.getApptRange(today,tomorrow);	
+ 			calDays = (LinkedList<CalDay>) dataHandler.getApptRange(today,tomorrow);
 
 			System.out.println("The number of appointments between "+ dateFormat.format(today.getTime()) +" (inclusive) and " + dateFormat.format(tomorrow.getTime()) +  " (exclusive) ");
 			for (int i = 0; i < calDays.size(); i++){
-				
+
 				CalDay calday= calDays.get(i);
-				
+
 				String str= calday.getFullInfomrationApp(calday);
 				System.out.println(str);
 
@@ -177,21 +177,21 @@ public class CalendarMain {
 					System.out.println("\n\n"+appt_.toString());
 				}
 			}
-// 	 		
+//
  	 		//Delete an appointment
- 			
+
  			dataHandler.deleteAppt(appt2);
- 			
- 			
+
+
      	/** instance of DataHandler **/
  		DataHandler dataHandler2 = new DataHandler("calendar2.xml",true);
-// 		
+//
 
- 		
+
  		dataHandler2.saveAppt(appt2);
 
 
          System.exit(0);
-		
+
 	}
 }
