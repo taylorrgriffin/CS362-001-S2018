a83542b2ffb93ec52b9df590007d14a0f0d9a853
Taylor Griffin <taylor.griffin.365@gmail.com>
2018-06-03 17:13:44 -0700

    fixed bug 2

--- projects/griftayl/Assignment-5/ahujae/Calendar/src/main/java/calendar/Appt.java
+++ projects/griftayl/Assignment-5/ahujae/Calendar/src/main/java/calendar/Appt.java
@@ -174,7 +174,7 @@ public class Appt{
 		else if (startMinute < 0 || startMinute > 59)
 			this.valid = false;
     // Bug #3 is here! <= 0 -> < 0
-		else if (startYear < 0)
+		else if (startYear <= 0)
 			this.valid = false;
 		else {
 			int NumDaysInMonth = CalendarUtil.NumDaysInMonth(startYear, startMonth - 1);
